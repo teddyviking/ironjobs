f8e6c4a938b3439592d1513c69b16ee5acfecfa2
Rafael Tejado <rtejado90@gmail.com>
2015-05-14 18:28:08 +0200

    Add tests for presence validation of email, location, name and last name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "when saving an user without required attributes" do
+  	user = User.new
+  	user.valid?
+  	it "raises an error when no email included" do
+  		expect(user.errors[:email].any?).to eq(true)
+  	end
+  	it "raises an error when no name included" do
+  		expect(user.errors[:name].any?).to eq(true)
+  	end
+  	it "raises an error when no last name included" do
+  		expect(user.errors[:last_name].any?).to eq(true)
+  	end
+  	it "raises an error when no location included" do
+  		expect(user.errors[:location].any?).to eq(true)
+  	end
+  end
 end
