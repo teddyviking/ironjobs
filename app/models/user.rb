2917cf28bdb16be176324b7f56985a786234b616
Rafael Tejado <rtejado90@gmail.com>
2015-05-15 09:58:46 +0200

    Pass test to validate presence and format of searching in students and companies

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,22 @@
 class User < ActiveRecord::Base
 	validates :first_name, :last_name, :email, :location, :role, presence: true
 	validates :email, uniqueness: true
-	validate :role_has_to_be_student_admin_or_company
+	validate :role_has_to_be_student_admin_or_company, :searching_is_present_in_students_and_companies
 
 
+
+
+	private
 
 	def role_has_to_be_student_admin_or_company
 		if role != "student" && role != "admin" && role != "company"
 			errors.add(:role, "must be student, admin or company")
+		end
+	end
+
+	def searching_is_present_in_students_and_companies
+		if role != "admin" && searching.nil?
+			errors.add(:searching, "must include searching status")
 		end
 	end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,37 +9,37 @@ RSpec.describe User, type: :model do
   	let(:create_admin) {create(:admin)}
   	let(:empty_user) {user = User.new}
   	
-  	it "has an error when no email included" do
+  	it "has an error if no email included" do
   		check_mandatory_attribute(:email, empty_user)
   	end
-  	it "has an error when no first name included" do
+  	it "has an error if no first name included" do
   		check_mandatory_attribute(:first_name, empty_user)
   	end
-  	it "has an error when no last name included" do
+  	it "has an error if no last name included" do
   		check_mandatory_attribute(:last_name, empty_user)
   	end
-  	it "has an error when no location included" do
+  	it "has an error if no location included" do
   		check_mandatory_attribute(:location, empty_user)
   	end
-  	it "has an error when no role included" do
+  	it "has an error if no role included" do
   		check_mandatory_attribute(:role, empty_user)
   	end
 
-  	it "has an error when a not valid role is included" do
+  	it "has an error if not valid role is included" do
   		user = build(:admin, role: "singer")
   		user.valid?
   		expect(user.errors[:role].any?).to eq(true)
   	end
 
-  	it "has an error when email already in use" do
+  	it "has an error if email already in use" do
   		user1 = create_student
   		user2 = build(:student, email: user1.email )
   		user2.valid?
   		expect(user2.errors[:email].any?).to eq(true)
   	end
 
-  	it "has an error when user not an admin and searching is not a boolean" do
-  		user = build(:student, searching: "yeah" )
+  	it "has an error if user not an admin and searching is empty" do
+  		user = build(:student, searching: nil )
   		user.valid?
   		expect(user.errors[:searching].any?).to eq(true)
   	end
