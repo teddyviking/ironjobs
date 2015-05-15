18f12314ce4265ede8987af8c3979d44b38576ed
Rafael Tejado <rtejado90@gmail.com>
2015-05-15 09:26:06 +0200

    Pass test to validate correct role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
 	validates :first_name, :last_name, :email, :location, :role, presence: true
 	validates :email, uniqueness: true
+	validate :role_has_to_be_student_admin_or_company
+
+
+
+	def role_has_to_be_student_admin_or_company
+		if role != "student" && role != "admin" && role != "company"
+			errors.add(:role, "must be student, admin or company")
+		end
+	end
 end
