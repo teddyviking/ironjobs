907aff51ed055727b7f5c9b1e2b87ffd0b900ea6
Rafael Tejado <rtejado90@gmail.com>
2015-05-15 15:28:31 +0200

    Pass basic tests of companies controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/students' => "students#index", as: :students
+  get '/companies' => "companies#index", as: :companies
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/controllers/companies_controller_spec.rb
+++ spec/controllers/companies_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe CompaniesController, type: :controller do
+	let(:create_company) {create(:company)}
 
+	describe "GET #index" do
+		before(:each) {get :index}
+
+		it "responds successfully with an HTTP 200 status code" do
+	      	expect(response).to be_success
+	      	expect(response).to have_http_status(200)
+	    end
+
+	    it "renders the index template" do
+	      expect(response).to render_template("index")
+	    end
+
+	end
 end
