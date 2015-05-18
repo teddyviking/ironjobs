5710cbbf2d8b7c37afc2f949ef079c673e2c2d87
Rafael Tejado <rtejado90@gmail.com>
2015-05-18 09:21:39 +0200

    Pass test to students#show when student doesn't exit return an array of students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,7 @@ class StudentsController < ApplicationController
 	def show
 		@student = User.find_by_id(params[:id])
 		unless @student
+			@students = User.where(role: "student")
 			flash.now[:alert] = "Student not found"
 			render "index", status: 301
 		end
