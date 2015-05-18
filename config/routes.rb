a53a41087ba685865c0042ac6fa69a5d1058dd9f
Rafael Tejado <rtejado90@gmail.com>
2015-05-18 17:05:02 +0200

    Update view of profile update of companies

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     get "/sign_up", to: "devise/registrations#new"
+    get "/edit_profile", to: "devise/registrations#edit"
   end
   root to: "students#index"
 
