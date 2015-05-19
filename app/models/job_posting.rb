c2e8908c938d7224bed4bcfc972b940ea8d26f38
Rafael Tejado <rtejado90@gmail.com>
2015-05-19 10:42:12 +0200

    Resolve very serious conflict with migrations, schema and models

--- app/models/job_posting.rb
+++
@@ -1,3 +0,0 @@
-class JobPosting < ActiveRecord::Base
-	belongs_to :company
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,62 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20150519074942) do
-=======
-ActiveRecord::Schema.define(version: 20150518163419) do
->>>>>>> a20eb9888b6c7a56baa12f3fa01011a18ef488dc
+ActiveRecord::Schema.define(version: 20150518144905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
-  create_table "applications", force: :cascade do |t|
-    t.integer "student_id"
-    t.integer "job_posting_id"
-  end
-
-  add_index "applications", ["job_posting_id"], name: "index_applications_on_job_posting_id", using: :btree
-  add_index "applications", ["student_id"], name: "index_applications_on_student_id", using: :btree
-
-  create_table "job_applications", force: :cascade do |t|
-    t.integer  "student_id"
-    t.integer  "job_posting_id"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-  end
-
-  add_index "job_applications", ["job_posting_id"], name: "index_job_applications_on_job_posting_id", using: :btree
-  add_index "job_applications", ["student_id"], name: "index_job_applications_on_student_id", using: :btree
-
-=======
->>>>>>> a20eb9888b6c7a56baa12f3fa01011a18ef488dc
-  create_table "job_postings", force: :cascade do |t|
-    t.text     "description"
-    t.string   "title"
-    t.string   "position"
-    t.string   "contract_type"
-    t.string   "category"
-    t.integer  "salary"
-    t.string   "location"
-    t.date     "deadline"
-    t.integer  "company_id"
-    t.string   "company_type"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-  end
-
-<<<<<<< HEAD
-  create_table "job_posts", force: :cascade do |t|
-    t.integer  "company_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "job_posts", ["company_id"], name: "index_job_posts_on_company_id", using: :btree
-
-=======
->>>>>>> a20eb9888b6c7a56baa12f3fa01011a18ef488dc
   create_table "users", force: :cascade do |t|
     t.string   "location"
     t.string   "first_name"
