9f72d9f941fec9abbcdbd170051dad5e5b112fd1
Rafael Tejado <rtejado90@gmail.com>
2015-05-19 17:04:38 +0200

    Add attributes to job posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150519102406) do
+ActiveRecord::Schema.define(version: 20150519145012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,8 +30,13 @@ ActiveRecord::Schema.define(version: 20150519102406) do
   create_table "job_posts", force: :cascade do |t|
     t.integer  "company_id"
     t.string   "company_type"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.text     "description"
+    t.string   "location"
+    t.integer  "salary"
+    t.string   "contract_type"
+    t.string   "position"
   end
 
   add_index "job_posts", ["company_id"], name: "index_job_posts_on_company_id", using: :btree
