8fc783a9bbd9364d53a330e2b7e580332ba9b3d8
Rafael Tejado <rtejado90@gmail.com>
2015-05-21 10:30:27 +0200

    Refactor url of job_post form into a helper

--- app/helpers/job_posts_helper.rb
+++ app/helpers/job_posts_helper.rb
@@ -1,2 +1,6 @@
 module JobPostsHelper
+
+	def dynamic_job_post_form_route(company, job_post)
+		job_post.new_record? ? company_job_posts_path(company) : company_job_post_path(company, job_post)
+	end
 end
