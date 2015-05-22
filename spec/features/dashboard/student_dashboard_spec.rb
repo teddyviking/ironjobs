7f6e5fbb380812761b957c48c363e6e5129d26d8
Rafael Tejado <rtejado90@gmail.com>
2015-05-22 20:09:26 +0200

    Add student dashboard

---
+++ spec/features/dashboard/student_dashboard_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+
+feature 'Student Dashboard' do
+	background do
+		@students = create_list(:student, 10)
+  		@user = @students.first
+	end
+
+  scenario 'A student logs in the app' do
+       visit new_user_session_path
+
+       fill_in("user_email", :with => @user.email)
+       fill_in("user_password", :with => @user.password)
+       click_on("Log in")
+
+       expect(current_path).to eq(root_path)
+
+       expect(page).to have_content("Welcome " + @user.first_name)
+       expect(source).to include(job_search_path)
+       expect(source).to include(company_search_path)
+       expect(source).to include(edit_student_path(@user))
+       expect(source).to include(student_applications_path(@user))
+  end
+end
